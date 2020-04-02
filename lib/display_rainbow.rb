7e520b1502b0b59c74733d265b0e7ef3264d687c
Erik Weir <erik.weir@key.me>
2020-04-02 19:22:00 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,6 @@
 def display_rainbow(colors)
+<<<<<<< HEAD
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+=======
+>>>>>>> 4cdc2469797e1b1c1bd59b3f751a3311a642c43e
 end
